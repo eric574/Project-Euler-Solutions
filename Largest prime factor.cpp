f4c9408953af72b3edce46d636aa96c87e88f319
Eric Liu <eric__1234@outlook.com>
2018-08-14 00:40:24 -0400

    Add files via upload

---
+++ Largest prime factor.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize "Ofast"
+#pragma GCC optimize "unroll-loops"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,mmx,popcnt,tune=native"
+// #include <bits/stdc++.h>
+#include "/Users/ericliu/Desktop/Competitive-Programming-Templates/stdc++.h"
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define ll long long
+#define ull unsigned long long
+#define INF 0x3f3f3f3f
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) < (b) ? (b) : (a)
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#ifdef DEBUG
+	#define D(x...) printf(x)
+#else
+	#define D(x...)
+#endif
+using namespace std;
+
+vi primes;
+
+inline void Sieve (ll MAXN) {
+	bool isPrime[MAXN];
+	memset(isPrime, 1, sizeof(isPrime));
+	isPrime[0] = isPrime[1] = 0;
+	for (int i=2; i<=MAXN; i++) {
+		if (isPrime[i]) {
+			for (int j=i*2; j<=MAXN; j+=i) isPrime[j] = 0;
+			primes.pb(i);
+		}
+	}
+}
+
+ull ans;
+
+int main () {
+	#ifdef NOT_DMOJ
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif // NOT_DMOJ
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	ull N = 600851475143;
+	Sieve(sqrt(N));
+	for (auto i : primes) if (N % i == 0) ans = i;
+	cout << ans << endl;
+	return 0;
+}
+
+/* 
+ * Look for:
+ * the exact constraints (multiple sets are too slow for n=10^6 :( ) 
+ * special cases (n=1?)
+ * overflow (ll vs int?)
+ * array bounds
+ */
